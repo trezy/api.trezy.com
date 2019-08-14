4c4bc1170c4f737239243ea6cfc04faac529c589
Trezy <tre@trezy.com>
2019-08-14 16:49:10 -0500

    refactor(/twitch): import credentials from ENV

--- src/routers/twitch.js
+++ src/routers/twitch.js
@@ -17,13 +17,30 @@ import firebaseCredentials from '../../firebase-credentials'
 
 // Local constants
 const {
-  TMDB_API_KEY,
-  TWILIO_AUTH_TOKEN,
-  TWILIO_PHONE_NUMBER,
-  TWILIO_SID,
+  FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  FIREBASE_AUTH_URI,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_CLIENT_ID,
+  FIREBASE_CLIENT_X509_CERT_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_PRIVATE_KEY_ID,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_TOKEN_URI,
+  FIREBASE_TYPE,
 } = process.env
 const firebaseApp = admin.initializeApp({
-  credential: admin.credential.cert(firebaseCredentials),
+  credential: admin.credential.cert({
+      type: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      project_id: FIREBASE_AUTH_URI,
+      private_key_id: FIREBASE_CLIENT_EMAIL,
+      private_key: FIREBASE_CLIENT_ID,
+      client_email: FIREBASE_CLIENT_X509_CERT_URL,
+      client_id: FIREBASE_PROJECT_ID,
+      auth_uri: FIREBASE_PRIVATE_KEY_ID,
+      token_uri: FIREBASE_PRIVATE_KEY,
+      auth_provider_x509_cert_url: FIREBASE_TOKEN_URI,
+      client_x509_cert_url: FIREBASE_TYPE,
+  }),
   databaseURL: 'https://trezy-core.firebaseio.com'
 })
 const database = firebaseApp.database()
